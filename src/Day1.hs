96cf71daf981901078d43a7a8819e9a60677103a
Craig Hupin <althar93@hotmail.com>
2025-12-01 13:07:38 +0000

    Reimplemented solutions to Day 1, in a more compact form, after witnessing far better solutions on reddit

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,14 +2,11 @@ module Day1 (answers) where
 
 import Parser
 
-
 testInputFile :: String
 testInputFile = "res/Day1_test_input.txt"
 
-
 inputFile :: String
 inputFile = "res/Day1_input.txt"
-
 
 parseRotations :: Parser [Int]
 parseRotations = many parseRotation where
@@ -19,17 +16,14 @@ parseRotations = many parseRotation where
        _ <- parseSpaces
        return $ if d == 'L' then -n else n 
 
-
 readInput :: String -> IO [Int]
 readInput file = do
     content <- readFile file
     return $ runParser parseRotations content
 
-
 -- | Turns the dial by a fixed number of clicks
 turn :: Int -> Int -> Int -> Int
 turn n x0 r = (x0 + r) `mod` n
-
 
 -- | Counts the number of rotations
 rotations :: Int -> Int -> Int
@@ -42,7 +36,6 @@ rotations x r = f + p where
        | otherwise       = 0
     x' = turn 100 x r
 
-
 -- | Answer for part 1
 part1 :: [Int] -> Int
 part1 yss = part1' 50 0 yss where
@@ -50,15 +43,6 @@ part1 yss = part1' 50 0 yss where
     part1' x n (y:ys) = part1' x' n' ys where
         x' = turn 100 x y
         n' = if x' == 0 then n + 1 else n
-
-
--- | Same as above but using a fold
-part1f :: [Int] -> Int
-part1f yss = snd $ foldl part1' (50, 0) yss where
-    part1' (x, n) y = (x', n') where
-        x' = turn 100 x y
-        n' = if x' == 0 then n + 1 else n
-
 
 -- | Answer for part 2
 part2 :: [Int] -> Int
@@ -68,19 +52,21 @@ part2 yss = part2' 50 0 yss where
         x' = turn 100 x y
         n' = n + rotations x y 
 
-
-rotateAndList :: Int -> [Int] -> IO ()
-rotateAndList _ []     = return ()
-rotateAndList x (y:ys) = do
-    let x' = turn 100 x y
-    let n  = rotations x y
-    putStrLn $ "The dial is rotated " ++ (show y) ++ " to point at " ++ (show x') ++ "; during this rotation, it points at 0 " ++ (show n) ++ " time(s)"
-    rotateAndList x' ys
-
+-- | Part 1 more compact & using 'scanl'
+part1' :: [Int] -> Int
+part1' yss = length $ filter (==0) xss where
+    xss = scanl (\x y -> (x + y) `mod` 100) 50 yss
+   
+-- | Part 2 more compact & using 'scanl'
+part2' :: [Int] -> Int
+part2' yss = sum $ zipWith delta xss yss where
+    xss = scanl (\x y -> (x + y) `mod` 100) 50 yss
+    delta :: Int -> Int -> Int
+    delta x y | y < 0     = ((100 - x) `mod` 100 - y) `div` 100
+              | otherwise = (x + y) `div` 100
 
 -- | Answers
 answers :: IO [Int]
 answers = do
     input <- readInput inputFile
-    rotateAndList 50 input
-    return [part1 input, part2 input]
+    return [part1' input, part2' input]
