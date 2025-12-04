c1291f670ae40e780e3ea743c33de6cef9926d72
Althar93 <althar93@hotmail.com>
2025-12-04 20:26:36 +0000

    Solution for Day 3

---
+++ src/Day3.hs
@@ -0,0 +1,62 @@
+module Day3 (answers) where
+
+import Parser
+
+import Data.Char
+
+-- | The test input
+testInputFile :: String
+testInputFile = "res/Day3_test_input.txt"
+
+-- | The actual input
+inputFile::String
+inputFile = "res/Day3_input.txt"
+
+-- | Parse multiple banks
+parseBanks :: Parser [[Int]]
+parseBanks = some parseBank where
+    parseBank = do
+        _ <- parseSpaces
+        ns <- some parseDigit
+        return $ map digitToInt ns
+
+-- | Reads the input
+readInput :: String -> IO [[Int]]
+readInput file = do
+    contents <- readFile file
+    return $ runParser parseBanks contents
+
+-- | Drops the tail elements
+dropTail :: Int -> [a] -> [a]
+dropTail n xs = take (length xs - n) xs
+
+-- | Generalised joltage calculation which takes the number of digits for the bank
+maxJoltageR :: Int -> [Int] -> Int
+maxJoltageR k b = sum $ zipWith (\n i -> n * 10^i) ls [k-1, k-2..0] where 
+    ls = maxJoltageR' k 0
+    maxJoltageR' 0  _ = []
+    maxJoltageR' k' i | k' == 99  = error $ show rb ++ " : " ++ show (n, i')
+                      | otherwise = n : maxJoltageR' (k' - 1) (i' + 1 + length lb) where
+        (n, i')  = localMaximum (dropTail (k' - 1) rb)
+        (lb, rb) = splitAt i b
+
+-- | Returns the local maximum for a list of numbers
+localMaximum :: [Int] -> (Int, Int)
+localMaximum b = localMaximum' (0,0) 0 b where
+    localMaximum' (m,k) _ []     = (m, k)
+    localMaximum' (m,k) i (n:ns) | n > m     = localMaximum' (n, i) (i+1) ns
+                                 | otherwise = localMaximum' (m, k) (i+1) ns
+
+-- | Answer for part 1
+answer1 :: [[Int]] -> Int
+answer1 bs = sum $ map (maxJoltageR 2) bs
+
+-- | Answer for part 2
+answer2 :: [[Int]] -> Int
+answer2 bs = sum $ map (maxJoltageR 12) bs
+
+-- | Answers
+answers :: IO [Int]
+answers = do
+    input <- readInput inputFile
+    return [answer1 input, answer2 input]
--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,10 @@ module Main (main) where
 import Common
 import Day1
 import Day2
+import Day3
 
 main :: IO ()
 main = do
     evaluateAndPrint "Day 1" Day1.answers
     evaluateAndPrint "Day 2" Day2.answers
+    evaluateAndPrint "Day 3" Day3.answers
